da81e66e4ac399ef055bb7b70b8eb7670d4c99e8
italo Garcia <lobo123445@outlook.es>
2021-08-21 05:33:50 -0600

    ejemplo

---
+++ clase 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace clase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.sortedListColletion();
+            Console.ReadKey();
+        }
+    }
+}
